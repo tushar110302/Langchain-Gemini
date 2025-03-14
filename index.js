c13cb98aa85dd8d98f23ed0aca0e4e4f63ac74de
Tushar Ranjan <thrrnjn1103@gmail.com>
2025-03-14 17:34:15 +0530

    zod installation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import dotenv from 'dotenv/config'
+import dotenv from 'dotenv/config';
+
 const llm = new ChatGoogleGenerativeAI({
     model: "gemini-2.0-flash",
     temperature: 0,
